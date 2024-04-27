a1c9fbdc03bd04b67fab36789b7ddf153813cde8
Kelvin Dwipangga <144301420+klvnd@users.noreply.github.com>
2024-04-27 14:57:14 +0700

    feat: Implement image selection from gallery without requiring media storage permission and display the selected image

--- app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
+++ app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
@@ -6,6 +6,9 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import com.dicoding.asclepius.R
 import com.dicoding.asclepius.databinding.ActivityMainBinding
 
@@ -16,15 +19,36 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        binding.galleryButton.setOnClickListener {
+            startGallery()
+        }
     }
 
     private fun startGallery() {
         // TODO: Mendapatkan gambar dari Gallery.
+        launcherGallery.launch(PickVisualMediaRequest(
+            ActivityResultContracts.PickVisualMedia.ImageOnly)
+        )
     }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        uri?.let {
+            currentImageUri = it
+            showImage()
+        }
+    }
+
 
     private fun showImage() {
         // TODO: Menampilkan gambar sesuai Gallery yang dipilih.
+        currentImageUri?.let { uri ->
+            binding.previewImageView.setImageURI(uri)
+        }
     }
 
     private fun analyzeImage() {
