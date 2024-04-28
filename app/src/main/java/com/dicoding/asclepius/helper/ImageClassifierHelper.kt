576b200185b437fef1b8b95ce4a1f9e95d8826fc
Kelvin Dwipangga <144301420+klvnd@users.noreply.github.com>
2024-04-28 18:00:25 +0700

    feat: Implementation of image classification using TensorFlow Lite on device

--- app/src/main/java/com/dicoding/asclepius/helper/ImageClassifierHelper.kt
+++ app/src/main/java/com/dicoding/asclepius/helper/ImageClassifierHelper.kt
@@ -1,18 +1,90 @@
 package com.dicoding.asclepius.helper
 
+import android.content.Context
 import android.graphics.Bitmap
-import android.media.Image
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
+import android.os.SystemClock
+import android.provider.MediaStore
+import android.util.Log
+import com.dicoding.asclepius.R
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.task.core.BaseOptions
+import org.tensorflow.lite.task.vision.classifier.Classifications
+import org.tensorflow.lite.task.vision.classifier.ImageClassifier
 
+@Suppress("DEPRECATION")
+class ImageClassifierHelper(
+    val context: Context,
+    val classifierListener: ClassifierListener?
+) {
 
-class ImageClassifierHelper() {
+    private var imageClassifier: ImageClassifier? = null
+
+    interface ClassifierListener {
+        fun onError(error: String)
+        fun onResults(
+            results: List<Classifications>?,
+            inferenceTime: Long
+        )
+    }
+
+    init {
+        setupImageClassifier()
+    }
 
     private fun setupImageClassifier() {
         // TODO: Menyiapkan Image Classifier untuk memproses gambar.
+        val optionsBuilder = ImageClassifier.ImageClassifierOptions.builder()
+            .setScoreThreshold(0.1f)
+            .setMaxResults(3)
+        val baseOptionBuilder = BaseOptions.builder()
+            .setNumThreads(4)
+        optionsBuilder.setBaseOptions(baseOptionBuilder.build())
+
+        try {
+            imageClassifier = ImageClassifier.createFromFileAndOptions(
+                context,
+                "cancer_classification.tflite",
+//                modelName,
+                optionsBuilder.build()
+            )
+        } catch (e: IllegalStateException) {
+            classifierListener?.onError(context.getString(R.string.image_clasifier_failed))
+            Log.e(TAG, e.message.toString())
+        }
+
     }
 
     fun classifyStaticImage(imageUri: Uri) {
         // TODO: mengklasifikasikan imageUri dari gambar statis.
+        if (imageClassifier == null) {
+            setupImageClassifier()
+        }
+
+        val imageProcessor = ImageProcessor.Builder().build()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val source = ImageDecoder.createSource(context.contentResolver, imageUri)
+            ImageDecoder.decodeBitmap(source)
+        } else {
+            MediaStore.Images.Media.getBitmap(context.contentResolver, imageUri)
+        }.copy(Bitmap.Config.ARGB_8888, true)?.let { bitmap ->
+            val tensorImage = imageProcessor.process(TensorImage.fromBitmap(bitmap))
+            var inferenceTime = SystemClock.uptimeMillis()
+            val results = imageClassifier?.classify(tensorImage)
+            inferenceTime = SystemClock.uptimeMillis() - inferenceTime
+            classifierListener?.onResults(
+                results,
+                inferenceTime
+            )
+        }
+    }
+
+    companion object {
+        private const val TAG = "ImageClassifierHelper"
     }
 
 }
