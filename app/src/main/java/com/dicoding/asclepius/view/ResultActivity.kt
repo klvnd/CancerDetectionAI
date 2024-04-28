6abc40b08b6b29da22b22f9a5253fe5882b5d666
Kelvin Dwipangga <144301420+klvnd@users.noreply.github.com>
2024-04-28 14:09:55 +0700

    feat: showing the image

--- app/src/main/java/com/dicoding/asclepius/view/ResultActivity.kt
+++ app/src/main/java/com/dicoding/asclepius/view/ResultActivity.kt
@@ -1,7 +1,8 @@
 package com.dicoding.asclepius.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.asclepius.R
 import com.dicoding.asclepius.databinding.ActivityResultBinding
 
@@ -12,7 +13,13 @@ class ResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         // TODO: Menampilkan hasil gambar, prediksi, dan confidence score.
+        val imageUriString = intent.getStringExtra("imageUri")
+        val imageUri = Uri.parse(imageUriString)
+        binding.resultImage.setImageURI(imageUri)
     }
 
 
